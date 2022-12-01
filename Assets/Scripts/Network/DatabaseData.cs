ce3fa8cec52d24e5d1156e3db4b917f306bfa539
playmaker1210 <hatvanitamas09@gmail.com>
2022-12-01 14:11:17 +0100

    database base done

--- Assets/Scripts/Network/DatabaseData.cs
+++ Assets/Scripts/Network/DatabaseData.cs
@@ -7,12 +7,18 @@ using UnityEngine.Networking;
 public class DatabaseData : MonoBehaviour
 {
     public TMP_InputField input;
+    private WriteFile wf;
 
+    private void Start() {
+        wf = FindObjectOfType<WriteFile>();
+    }
 
-    public void GetData() => StartCoroutine(GetData_Coroutine());
-    public void PostData() => StartCoroutine(PostData_Coroutine());
+    public void GetPlayerData() => StartCoroutine(IGetPlayerData());
+    public void PostNewPlayerData() => StartCoroutine(IPostNewPlayerData());
+    public void PostNewScoreData() => StartCoroutine(IPostNewScoreData());
+    public void PostNewPalyaData() => StartCoroutine(IPostNewPalyaData());
 
-    IEnumerator GetData_Coroutine() {
+    private IEnumerator IGetPlayerData() {
         input.text = "Loading...";
 
         string uri = "http://localhost:3000/player";
@@ -28,7 +34,7 @@ public class DatabaseData : MonoBehaviour
         }
     }
 
-    IEnumerator PostData_Coroutine() {
+    private IEnumerator IPostNewPlayerData() {
         input.text = "loading...";
 
         string uri = "http://localhost:3000/newplayer";
@@ -37,10 +43,66 @@ public class DatabaseData : MonoBehaviour
         formData.Add(new MultipartFormDataSection("field1=player_name"));
         formData.Add(new MultipartFormFileSection("bevitel1", "thewarrior1210"));*/
 
-        WWWForm form = new WWWForm();
-        form.AddField("bevitel1","thewarrior1210");
+        wf.writeUserName("newtesztUsername");
 
-        using(UnityWebRequest request = UnityWebRequest.Post(uri, form)) {
+        WWWForm form = new WWWForm();
+        //form.AddField("bevitel1","");
+
+        using(UnityWebRequest request = UnityWebRequest.Post(uri, "")) {
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError || request.isHttpError) { //lecserelni majd
+                input.text = request.error;
+            } else {
+                input.text = request.downloadHandler.text;
+            }
+        }
+    }
+
+
+    private IEnumerator IPostNewScoreData() {
+        input.text = "loading...";
+
+        string uri = "http://localhost:3000/newscore";
+
+        /*List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("field1=player_name"));
+        formData.Add(new MultipartFormFileSection("bevitel1", "thewarrior1210"));*/
+
+        wf.WritePlayerid(2);
+        wf.WritePalyaid(1);
+        wf.WriteScore(10);
+        wf.WriteTime("00:01:24");
+
+        WWWForm form = new WWWForm();
+        //form.AddField("bevitel1", "");
+
+        using (UnityWebRequest request = UnityWebRequest.Post(uri, "")) {
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError || request.isHttpError) { //lecserelni majd
+                input.text = request.error;
+            } else {
+                input.text = request.downloadHandler.text;
+            }
+        }
+    }
+
+    private IEnumerator IPostNewPalyaData() {
+        input.text = "loading...";
+
+        string uri = "http://localhost:3000/newpalya";
+
+        /*List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("field1=player_name"));
+        formData.Add(new MultipartFormFileSection("bevitel1", "thewarrior1210"));*/
+
+        wf.WritePalyaNev("Easy2");
+
+        WWWForm form = new WWWForm();
+        //form.AddField("bevitel1", "");
+
+        using (UnityWebRequest request = UnityWebRequest.Post(uri, "")) {
             yield return request.SendWebRequest();
 
             if (request.isNetworkError || request.isHttpError) { //lecserelni majd
--- Assets/Scripts/Network/WriteFile.cs
+++ Assets/Scripts/Network/WriteFile.cs
@@ -5,13 +5,45 @@ using System.IO;
 
 public class WriteFile : MonoBehaviour
 {
-    
-
+    //player
     public void writeUserName(string username) {
-        StreamWriter writer = new StreamWriter(@"adat.txt", false);
+        StreamWriter writer = new StreamWriter(@"../backend/username.txt", false);
         writer.Write(username);
         writer.Close();
     }
-    
-    
+    //player vege
+
+    //Score
+    public void WritePlayerid(int playerid) {
+        StreamWriter writer = new StreamWriter(@"../backend/playerid.txt", false);
+        writer.Write(playerid);
+        writer.Close();
+    }
+
+    public void WritePalyaid(int palyaid) {
+        StreamWriter writer = new StreamWriter(@"../backend/palyaid.txt", false);
+        writer.Write(palyaid);
+        writer.Close();
+    }
+
+    public void WriteScore(int score) {
+        StreamWriter writer = new StreamWriter(@"../backend/score.txt", false);
+        writer.Write(score);
+        writer.Close();
+    }
+
+    public void WriteTime(string time) {
+        StreamWriter writer = new StreamWriter(@"../backend/time.txt", false);
+        writer.Write(time);
+        writer.Close();
+    }
+    //Score vege
+
+    //Palya
+    public void WritePalyaNev(string palyanev) {
+        StreamWriter writer = new StreamWriter(@"../backend/ppalyanev.txt", false);
+        writer.Write(palyanev);
+        writer.Close();
+    }
+    //palya vege
 }
